619d1661b54629fce3af2d8ec533fe83c9ac53fe
deathtroll22 <deathtroll@laposte.net>
2024-01-31 16:45:35 +0100

    Creation du contexxt VariablesContext,  avec le fetch et le get by id

--- shared/contexts/VariableContext.tsx
+++ shared/contexts/VariableContext.tsx
@@ -1,62 +1,62 @@
 import React, {createContext, PropsWithChildren, useContext, useState} from "react";
-import {ActivityType} from "../types/ActivityType";
+import {VariableType} from "../types/VariableType";
 import {useEffect} from "react";
 
-type ActivityContextType = {
-    activities: ActivityType[],
-    addActivity: (activity: ActivityType) => void,
-    removeActivity: (id: string) => void,
-    getActivityById: (id: string) => ActivityType | undefined
-}
-
-const ActivityContext= createContext<ActivityContextType | null>(null)
-
-const VariableContextProvider = ({ children }: PropsWithChildren<{}>) => {
-    const [activities, setActivities] = useState<ActivityType[]>([])
-
-    const fetchActivitiesFromAPI = async () => {
-        try {
-            const response = await fetch("https://api.pebble.solutions/v5/activity/"); // Remplacez URL_DE_VOTRE_API par l'URL de votre API
-            const data = await response.json();
-            setActivities(data);
-        } catch (error) {
-            console.error("Erreur lors de la récupération des activités depuis l'API:", error);
-        }
+type VariableContextType = {
+    variables: VariableType[],
+    addVariable: (variable: VariableType) => void,
+    removeVariable: (id: string) => void,
+    getVariableById: (id: string) => VariableType | undefined
+  }
+  
+  const VariableContext = createContext<VariableContextType | null>(null);
+  
+  const VariableContextProvider = ({ children }: PropsWithChildren<{}>) => {
+    const [variables, setVariables] = useState<VariableType[]>([]);
+  
+    const fetchVariablesFromAPI = async () => {
+      try {
+        const response = await fetch("https://api.pebble.solutions/v5/metric/variable/"); // Remplacez URL_DE_VOTRE_API par l'URL de votre API
+        const data = await response.json();
+        setVariables(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des variables depuis l'API:", error);
+      }
     }
-
+  
     useEffect(() => {
-        fetchActivitiesFromAPI();
+      fetchVariablesFromAPI();
     }, []); 
-
-    const addActivity = (activity: ActivityType) => {
-        setActivities([...activities, activity])
+  
+    const addVariable = (variable: VariableType) => {
+      setVariables([...variables, variable]);
     }
-
-    const removeActivity = (id: string) => {
-        setActivities((prev) => {
-            return prev.filter(e => e._id !== id)
-        })
+  
+    const removeVariable = (id: string) => {
+      setVariables((prev) => {
+        return prev.filter(e => e._id !== id);
+      });
     }
-
-    const getActivityById = (id: string) => {
-        return activities.find(e => e._id === id)
+  
+    const getVariableById = (id: string) => {
+      return variables.find(e => e._id === id);
     }
-
+  
     return (
-        <ActivityContext.Provider value={{activities, addActivity, removeActivity, getActivityById}}>
-            {children}
-        </ActivityContext.Provider>
+      <VariableContext.Provider value={{ variables, addVariable, removeVariable, getVariableById }}>
+        {children}
+      </VariableContext.Provider>
     )
-}
-
-export default VariableContextProvider
-
-export const useActivityContext = () => {
-    const context = useContext(ActivityContext)
-
+  }
+  
+  export default VariableContextProvider;
+  
+  export const useVariableContext = () => {
+    const context = useContext(VariableContext);
+  
     if (!context) {
-        throw new Error("useActivityContext must be used inside the ActivityContextProvider")
+      throw new Error("useVariableContext must be used inside the VariableContextProvider");
     }
-
-    return context
-}
+  
+    return context;
+  }
