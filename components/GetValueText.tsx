2e13b7bd567ada8bf916e6824b6530fc9a1b71b9
emarthkopp <emarth@outlook.com>
2024-02-01 16:09:18 +0100

    premeirs composants tunnel réponse

--- components/GetValueText.tsx
+++ components/GetValueText.tsx
@@ -1,35 +1,55 @@
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
-import {globalStyles} from "../shared/globalStyles";
+import React, { useState } from "react";
+import { View, Text, TextInput, Alert, StyleSheet } from 'react-native';
+import { globalStyles } from "../shared/globalStyles";
 
-
+// Type définissant les propriétés attendues par le composant
 type ResponseTextType = {
-    varText: string
-
+  varText: {
+    _id: string;
+    label: string;
+    min_length: number;
+    max_length: number;
+    question: string;
+    mandatory: boolean;
+  };
 }
 
+// Composant de réponse textuelle
+const ResponseText: React.FC<ResponseTextType> = ({ varText }) => {
+  console.log(varText, 'varText');
 
-export default function ResponseText({ varText }: ResponseTextType) {
-    console.log(varText, ' varText')
-  
-    
-    
+  // State pour suivre la réponse de l'utilisateur
+  const [response, setResponse] = React.useState({
+    'id': varText._id,
+    'label': varText.label,
+    'value': ''
+  });
+
+  // Fonction appelée lorsqu'il y a un changement dans le TextInput
+  const handleChange = (text: string) => {
+    // Met à jour la réponse dans le state
+    if (text.length <= varText.max_length) {
+      setResponse(prev => ({ ...prev, value: text }));
+    } else {
+      Alert.alert(`La réponse ne peut pas dépasser ${varText.max_length} caractères`);
+    }
+  };
+
   return (
     <View>
-     
-        <Text>
-            {varText}
-        </Text>
-        <Text>{"Toto"}</Text>
-        <TextInput
-         
-          placeholder="Saisissez votre réponse ici "
-          
-    
-          value={""}
-        />
-      
+      <Text>
+        {varText.question}
+      </Text>
+      <TextInput
+        // style={globalStyles.input}
+        placeholder={`Saisissez votre réponse ici (max ${varText.max_length} caractères)`}
+        value={response.value}
+        onChangeText={(text) => handleChange(text)}
+      />
     </View>
   );
-}
+};
+
+export default ResponseText;
 
 
