302dbc8fb45b1a2726be19e214e9ac4f31faa114
Mari LEHOUX <mari.lehoux@gmail.com>
2024-04-03 15:08:32 +0200

    tunnel de validation, les booleen à false apparaissaient comme non renseigné

--- shared/globalStyles.ts
+++ shared/globalStyles.ts
@@ -398,7 +398,7 @@ export const globalStyles = StyleSheet.create({
         borderRadius: variables.borderRadius[2],
         borderWidth: 1,
         borderColor: variables.color.lightGrey,
-        backgroundColor: variables.color.dark,
+        backgroundColor: variables.color.lightGrey,
         fontSize: variables.fontSize[1],
         color: variables.color.white,
         marginVertical: variables.contentMargin[1],
